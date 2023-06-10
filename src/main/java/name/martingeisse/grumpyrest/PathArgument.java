e027699488945ca4d8aec31b748fa617ab13d8f5
Martin Geisse <martingeisse@googlemail.com>
2023-06-10 11:15:24 +0200

    added copyright headers to all Java files

--- src/main/java/name/martingeisse/grumpyrest/PathArgument.java
+++ src/main/java/name/martingeisse/grumpyrest/PathArgument.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023 Martin Geisse
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package name.martingeisse.grumpyrest;
 
 import com.google.gson.reflect.TypeToken;
--- src/main/java/name/martingeisse/grumpyrest/servlet/RestServlet.java
+++ src/main/java/name/martingeisse/grumpyrest/servlet/RestServlet.java
@@ -1,16 +1,9 @@
-//
-// ========================================================================
-// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
-//
-// This program and the accompanying materials are made available under the
-// terms of the Eclipse Public License v. 2.0 which is available at
-// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
-// which is available at https://www.apache.org/licenses/LICENSE-2.0.
-//
-// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-// ========================================================================
-//
-
+/*
+ * Copyright (c) 2023 Martin Geisse
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package name.martingeisse.grumpyrest.servlet;
 
 import name.martingeisse.grumpyrest.RestApi;
