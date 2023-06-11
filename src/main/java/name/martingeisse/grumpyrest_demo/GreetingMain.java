38f7f9d8bb4e212b578fd4755d181a63c3d56812
Martin Geisse <martingeisse@googlemail.com>
2023-06-11 20:48:18 +0200

    added "greeting" example

---
+++ src/main/java/name/martingeisse/grumpyrest_demo/GreetingMain.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2023 Martin Geisse
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package name.martingeisse.grumpyrest_demo;
+
+import name.martingeisse.grumpyjson.builtin.helper_types.OptionalField;
+import name.martingeisse.grumpyrest.RestApi;
+import name.martingeisse.grumpyrest.servlet.RestServlet;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class GreetingMain {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+
+        @SuppressWarnings("resource") ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.setConnectors(new Connector[]{connector});
+
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.addServlet(MyServlet.class, "/");
+
+        server.setHandler(new HandlerList(context, new DefaultHandler()));
+        server.start();
+        server.join();
+    }
+
+    public static class MyServlet extends RestServlet {
+
+        public MyServlet() {
+            super(buildApi());
+        }
+
+        record MakeGreetingRequest(String name, OptionalField<String> addendum) {}
+        record MakeGreetingResponse(String greeting) {}
+
+        private static RestApi buildApi() {
+            RestApi api = new RestApi();
+            api.addRoute("/make-greeting", requestCycle -> {
+                MakeGreetingRequest request = requestCycle.parseBody(MakeGreetingRequest.class);
+                if (request.addendum.isPresent()) {
+                    return new MakeGreetingResponse("Hello, " + request.name + "! " + request.addendum.getValue());
+                } else {
+                    return new MakeGreetingResponse("Hello, " + request.name + "!");
+                }
+            });
+            return api;
+        }
+
+    }
+
+}
