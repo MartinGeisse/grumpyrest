d5497fbc4488f10f8700d84e4f940b1587218b9a
Martin Geisse <martingeisse@googlemail.com>
2023-06-12 19:01:24 +0200

    added dummy tests so JUnit won't complain about missing test folders

---
+++ grumpyrest/src/test/java/name/martingeisse/grumpyrest/DummyTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Martin Geisse
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package name.martingeisse.grumpyrest;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void testOk() {
+    }
+
+}
