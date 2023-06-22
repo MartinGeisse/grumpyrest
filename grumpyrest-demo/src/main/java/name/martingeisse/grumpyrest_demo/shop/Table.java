9dc7a63aaed9d9ef0e10653e2dac0eb86e7e60f1
Martin Geisse <martingeisse@googlemail.com>
2023-06-22 17:40:45 +0200

    moved shop demo to subpackage

--- grumpyrest-demo/src/main/java/name/martingeisse/grumpyrest_demo/shop/Table.java
+++ grumpyrest-demo/src/main/java/name/martingeisse/grumpyrest_demo/shop/Table.java
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package name.martingeisse.grumpyrest_demo;
+package name.martingeisse.grumpyrest_demo.shop;
 
 import name.martingeisse.grumpyrest.response.FinishRequestException;
 import name.martingeisse.grumpyrest.response.standard.StandardErrorResponse;
