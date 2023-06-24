8476edefb5a0f96bc2772292b70e1d386306bee8
Martin Geisse <martingeisse@googlemail.com>
2023-06-24 18:18:24 +0200

    javadoc / cleanup

--- grumpyrest/src/main/java/name/martingeisse/grumpyrest/request/stringparser/ParseFromStringService.java
+++ grumpyrest/src/main/java/name/martingeisse/grumpyrest/request/stringparser/ParseFromStringService.java
@@ -8,7 +8,13 @@ import java.lang.reflect.Type;
 public interface ParseFromStringService {
 
     /**
-     * Parses value from a string.
+     * Parses a string to obtain a value of the specified type.
+     *
+     * @param text the text to parse
+     * @param type the type to convert to
+     * @return the converted value
+     * @throws FromStringParserException if conversion fails because the text does not conform to the expected
+     * format according to the type to convert to
      */
     Object parseFromString(String text, Type type) throws FromStringParserException;
 
