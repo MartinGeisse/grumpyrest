e95fd3ae9682f09d650e3a2a162f38e2f69dab8e
Martin Geisse <martingeisse@googlemail.com>
2023-06-24 14:24:23 +0200

    javadoc / cleanup

--- grumpyrest/src/main/java/name/martingeisse/grumpyrest/RestApi.java
+++ grumpyrest/src/main/java/name/martingeisse/grumpyrest/RestApi.java
@@ -6,14 +6,19 @@
  */
 package name.martingeisse.grumpyrest;
 
+import com.google.gson.JsonElement;
 import name.martingeisse.grumpyjson.JsonEngine;
+import name.martingeisse.grumpyjson.JsonRegistry;
+import name.martingeisse.grumpyjson.JsonTypeAdapter;
 import name.martingeisse.grumpyrest.request.HttpMethod;
 import name.martingeisse.grumpyrest.request.path.Path;
 import name.martingeisse.grumpyrest.request.querystring.QuerystringParserRegistry;
+import name.martingeisse.grumpyrest.response.FinishRequestException;
 import name.martingeisse.grumpyrest.response.NoResponseFactoryException;
 import name.martingeisse.grumpyrest.response.Response;
 import name.martingeisse.grumpyrest.response.ResponseFactory;
 import name.martingeisse.grumpyrest.response.ResponseFactoryRegistry;
+import name.martingeisse.grumpyrest.response.ResponseValueWrapper;
 import name.martingeisse.grumpyrest.response.standard.IdentityResponseFactory;
 import name.martingeisse.grumpyrest.response.standard.JsonResponseFactory;
 import name.martingeisse.grumpyrest.response.standard.NullResponseFactory;
@@ -22,6 +27,7 @@ import name.martingeisse.grumpyrest.request.stringparser.FromStringParser;
 import name.martingeisse.grumpyrest.request.stringparser.FromStringParserRegistry;
 import name.martingeisse.grumpyrest.request.stringparser.standard.IntegerFromStringParser;
 import name.martingeisse.grumpyrest.request.stringparser.standard.StringFromStringParser;
+import name.martingeisse.grumpyrest.response.standard.StatusOnlyResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +35,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is the main Entry point into grumpyrest. It is used by the application to define the REST API in terms
+ * of endpoints and supported data types.
+ */
 public final class RestApi {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestApi.class);
@@ -39,6 +49,26 @@ public final class RestApi {
     private final FromStringParserRegistry fromStringParserRegistry = new FromStringParserRegistry();
     private final QuerystringParserRegistry querystringParserRegistry = new QuerystringParserRegistry(fromStringParserRegistry);
 
+    /**
+     * Constructor.
+     * <p>
+     * No API routes will be registered by default, not even a route for the root path. You can add routes / handlers
+     * after construction. You can also add a route that handles all unmatched requests, to implement a custom "not
+     * found" response -- but make sure to add this route last, because it should match all requests, and then no
+     * other route after than can be matched anymore.
+     * <p>
+     * Unmatched requests: By default, no fallback route for unmatched requests is present. Instead, the API handles
+     * such requests internally by sending a standard 404 response that cannot be changed. This is just a formal
+     * way to specify that a manually added catch-all route is the one and only way to implement custom "not found"
+     * behavior.
+     * <p>
+     * From-string parsers, JSON Type adapters and response factories: After construction, this API object will have
+     * default from-string parsers (for path / querystring parameters) and default response factories registered. It
+     * will also use a newly created {@link JsonRegistry} which has default type adapters registered. All of these
+     * defaults can be removed to support special cases that, for example, use different rules / formats or require a
+     * more lenient parser. In normal cases, the defaults support standard types out of the box and support for
+     * application types can be added without removing any of the standard implementations.
+     */
     public RestApi() {
 
         // HTTP response factories
@@ -54,34 +84,130 @@ public final class RestApi {
 
     // region configuration
 
+    /**
+     * Adds a route to handle requests. This route can only be matched by requests that do not match any previously
+     * added route, and takes precedence over any route added later.
+     * <p>
+     * The {@link Route} object defines both which requests it will match as well as the handler to invoke for such
+     * requests.
+     *
+     * @param route the route to add
+     */
     public void addRoute(Route route) {
         routes.add(route);
     }
 
+    /**
+     * Adds a route to handle requests. This route can only be matched by requests that do not match any previously
+     * added route, and takes precedence over any route added later.
+     *
+     * @param method the HTTP method to match
+     * @param path the path to match
+     * @param handler the handler to invoke
+     */
     public void addComplexRoute(HttpMethod method, Path path, ComplexHandler handler) {
         addRoute(new Route(method, path, handler));
     }
 
+    /**
+     * Adds a route to handle requests. This route can only be matched by requests that do not match any previously
+     * added route, and takes precedence over any route added later.
+     *
+     * @param method the HTTP method to match
+     * @param path the path to match
+     * @param handler the handler to invoke
+     */
     public void addComplexRoute(HttpMethod method, String path, ComplexHandler handler) {
         addRoute(new Route(method, path, handler));
     }
 
+    /**
+     * Adds a route to handle requests. This route can only be matched by requests that do not match any previously
+     * added route, and takes precedence over any route added later.
+     *
+     * @param method the HTTP method to match
+     * @param path the path to match
+     * @param handler the handler to invoke
+     */
     public void addRoute(HttpMethod method, Path path, SimpleHandler handler) {
         addRoute(new Route(method, path, handler));
     }
 
+    /**
+     * Adds a route to handle requests. This route can only be matched by requests that do not match any previously
+     * added route, and takes precedence over any route added later.
+     *
+     * @param method the HTTP method to match
+     * @param path the path to match
+     * @param handler the handler to invoke
+     */
     public void addRoute(HttpMethod method, String path, SimpleHandler handler) {
         addRoute(new Route(method, path, handler));
     }
 
+    /**
+     * Returns a snapshot of the currently present routes.
+     *
+     * @return the routes, as an immutable snapshot
+     */
     public List<Route> getRoutes() {
-        return routes;
+        return List.copyOf(routes);
     }
 
+    /**
+     * Adds a {@link ResponseFactory} to support new kinds of response values. This includes exception types for
+     * which a specific response shall be generated (by default, exceptions just cause a standard 500 response).
+     * <p>
+     * Note that support for new JSON-able types should not be implemented as a response factory, but by
+     * registering a {@link JsonTypeAdapter} with the {@link JsonRegistry} returned by {@link #getJsonEngine()} /
+     * {@link JsonEngine#getRegistry()}.
+     * <p>
+     * Since most responses are expected to be JSON-based, custom response factories are rare. If you think you need
+     * one, consider these alternatives:
+     * <ul>
+     *     <li>A custom {@link JsonTypeAdapter}, as mentioned above, to achieve a custom response format</li>
+     *     <li>A {@link StandardErrorResponse} to send error responses in standard JSON format</li>
+     *     <li>Building and returning a {@link JsonElement} (in case your response format does not have to
+     *       be abstracted and re-usable</li>
+     *     <li>Throwing a {@link FinishRequestException} as a means of control flow, to stop handling the requests and
+     *       to return any of the above to the framework immediately</li>
+     *     <li>A {@link StatusOnlyResponse} for custom HTTP-level errors</li>
+     *     <li>Returning a {@link Response} directly (in case you need a custom response, but it does not have to be
+     *       abstracted and re-usable).</li>
+     * </ul>
+     * One example where none of these is sufficient and you'll want a custom response factory is when you want to
+     * define a custom RedirectException that sens a 302 redirect to the client. Reasons you need a custom response
+     * factory in this case are:
+     * <ul>
+     *     <li>The problem is not JSON-related, so neither a {@link JsonTypeAdapter} nor {@link JsonElement} will help</li>
+     *     <li>The {@link StandardErrorResponse} format is not related to the HTTP status nor the Location header,
+     *       and if the redirect works, the response body should be irrelevant to the client</li>
+     *     <li>The {@link FinishRequestException} actually helps with the control flow, but not with generating the
+     *       response. You might want to derive RedirectException from {@link FinishRequestException}, or use a
+     *       Redirection response wrapped by a {@link FinishRequestException} instead (more work for the caller,
+     *       though), or have the RedirectException implement {@link ResponseValueWrapper} to kind-of make it its
+     *       own response factory. But let's say, you don't want that for some reason.</li>
+     *     <li>A {@link StatusOnlyResponse} only sets the status code. It won't add a Location header.</li>
+     *     <li>A {@link Response} puts the burden of generating a correct 302 redirect on the caller.</li>
+     * </ul>
+     * In this case, you'll want to make RedirectResponse as abstract and as convenient for the caller as possible.
+     * For example, you might define it in such a way that the caller does not have to specify the full URL to redirect
+     * to, but only higher-level information from which the URL is derived. A custom {@link ResponseFactory} is then
+     * a good way to generate a {@link Response} for that exception.
+     *
+     * @param responseFactory the response factory to register
+     */
     public void addResponseFactory(ResponseFactory responseFactory) {
         responseFactoryRegistry.add(responseFactory);
     }
 
+    /**
+     * Getter method for the registry for custom response factories. See {@link #addResponseFactory(ResponseFactory)}
+     * for some information on when such a factory helps. Getting the registry itself is necessary when you want to
+     * remove the default response factories present after construction.
+     *
+     * @return the response factory registry
+     */
     public ResponseFactoryRegistry getResponseFactoryRegistry() {
         return responseFactoryRegistry;
     }
