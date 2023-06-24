4703b04f8719283b8e71ea55ef53bbd91f5f9df0
Martin Geisse <martingeisse@googlemail.com>
2023-06-24 14:57:54 +0200

    javadoc / cleanup

--- grumpyrest/src/main/java/name/martingeisse/grumpyrest/Route.java
+++ grumpyrest/src/main/java/name/martingeisse/grumpyrest/Route.java
@@ -13,16 +13,66 @@ import name.martingeisse.grumpyrest.request.stringparser.ParseFromStringService;
 
 import java.util.List;
 
+/**
+ * A route is, conceptually, a set of criteria that determine whether the route matches a specific requests, as well
+ * as a handler that defines what to do for requests that match. The {@link RestApi} handles a request by picking the
+ * first route that matches, then handing the request to that route. Routes are the main mechanism to distinguish
+ * requests by HTTP method and path.
+ * <p>
+ * In a more practical sense,
+ * <ul>
+ *     <li>routes always distinguish by HTTP method. That is, you cannot (currently) define a route that matches
+ *     multiple or all HTTP methods. You'll have to add multiple routes for that.</li>
+ *     <li>each route uses a path (actually, a path pattern) and only handles requests that match this path. The
+ *     path can contain literal path segments (strings that must match the request exactly) as well as path
+ *     variables (a.k.a. path parameters) that will match any path segment from the request, and also capture the
+ *     provided path segment for use in the handler. This is used mainly for paths that contain entity IDs.</li>
+ *     <li>the handler is simply a callback that gets called if the route matches. There are two possible interfaces
+ *     to implement as a handler, {@link SimpleHandler} and {@link ComplexHandler}. You should implement
+ *     {@link SimpleHandler} if possible, and {@link ComplexHandler} if necessary, because it reduces coupling and
+ *     simplifies testing and mocking. Refer to these interfaces for details</li>
+ * </ul>
+ * </p>
+ * The canonical constructor takes a {@link ComplexHandler} because this is the more general case.
+ *
+ * @param method the HTTP method to match
+ * @param path the path pattern to match. May include path parameters.
+ * @param handler the handler to invoke for requests that match this route
+ */
 public record Route(HttpMethod method, Path path, ComplexHandler handler) {
 
+    /**
+     * This constructor specifies the path pattern as a string instead of a {@link Path} object. Leading and trailing
+     * slashes are ignored. A path parameter is specified as a path segment that starts with a ':' character.
+     *
+     * @param method the HTTP method to match
+     * @param path the path pattern to match. May include path parameters.
+     * @param handler the handler to invoke for requests that match this route
+     */
     public Route(HttpMethod method, String path, ComplexHandler handler) {
         this(method, Path.parse(path), handler);
     }
 
+    /**
+     * Constructor for a {@link SimpleHandler}.
+     *
+     * @param method the HTTP method to match
+     * @param path the path pattern to match. May include path parameters.
+     * @param handler the handler to invoke for requests that match this route
+     */
     public Route(HttpMethod method, Path path, SimpleHandler handler) {
         this(method, path, (RequestCycle requestCycle) -> handler.handle(requestCycle.getHighlevelRequest()));
     }
 
+    /**
+     * Constructor for a {@link SimpleHandler} which also specifies the path pattern as a string instead of a
+     * {@link Path} object. Leading and trailing slashes are ignored. A path parameter is specified as a path segment
+     * that starts with a ':' character.
+     *
+     * @param method the HTTP method to match
+     * @param path the path pattern to match. May include path parameters.
+     * @param handler the handler to invoke for requests that match this route
+     */
     public Route(HttpMethod method, String path, SimpleHandler handler) {
         this(method, Path.parse(path), handler);
     }
--- grumpyrest/src/main/java/name/martingeisse/grumpyrest/RouteMatchResult.java
+++ grumpyrest/src/main/java/name/martingeisse/grumpyrest/RouteMatchResult.java
@@ -4,8 +4,20 @@ import name.martingeisse.grumpyrest.request.PathArgument;
 
 import java.util.List;
 
+/**
+ * The result returned by {@link RestApi#match(RequestCycle)} and {@link Route#match(RequestCycle)}.
+ *
+ * @param route the route that was matched
+ * @param pathArguments arguments that were bound to path parameters
+ */
 public record RouteMatchResult(Route route, List<PathArgument> pathArguments) {
 
+    /**
+     * Compact constructor.
+     *
+     * @param route the route that was matched
+     * @param pathArguments arguments that were bound to path parameters
+     */
     public RouteMatchResult {
         pathArguments = List.copyOf(pathArguments);
     }
