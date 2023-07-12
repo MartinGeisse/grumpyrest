dc1905e8128abc757a7fdf2058daa1ba20e106f2
Martin Geisse <martingeisse@googlemail.com>
2023-07-12 17:37:28 +0200

    ...

--- scripts/online-javadoc.sh
+++
@@ -1,6 +0,0 @@
-./gradlew clean javadoc
-cd ../martingeisse.github.io/javadoc
-rm -rf grumpyrest
-mkdir grumpyrest
-cd grumpyrest
-cp -R ../../../grumpyrest/grumpyrest/build/docs/javadoc/* .
--- scripts/wiki.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-cd grumpyrest-wiki
-rm -rf doc
-cp ../grumpyrest/README.md Home.md
-cp -R ../grumpyrest/doc .
-git add -A
-git commit -m sync
-git push
