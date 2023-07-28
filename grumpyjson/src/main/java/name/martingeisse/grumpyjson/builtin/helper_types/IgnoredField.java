0fc1ac94419a3fec3e77023168ded6524cf4f6c6
Martin Geisse <martingeisse@googlemail.com>
2023-07-28 17:53:59 +0200

    IgnoredField + converter, to ignore fields in JSON

---
+++ grumpyjson/src/main/java/name/martingeisse/grumpyjson/builtin/helper_types/IgnoredField.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 Martin Geisse
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package name.martingeisse.grumpyjson.builtin.helper_types;
+
+/**
+ * This can be used to implement a property that gets ignored during deserialization, and is absent during
+ * serialization. Such a property obviously does not add any information to the data structure, but might be useful to
+ * maintain compatibility with a field that was used in previous versions.
+ * <p>
+ * In JSON, an IgnoredField can only be used in a record (actually any place that allows vanishable fields), not in
+ * places such as a list, because there is no way to ignore a field or serialize to an absent field there.
+ * <p>
+ * A shared instance is provided to reduce memory usage, but creating new instances is fine as well.
+ */
+public record IgnoredField() {
+
+    /**
+     * The shared instance of this class. Can be used to avoid memory usage from creating a new instance. This is not
+     * a singleton because it is possible to create other instances.
+     */
+    public static final IgnoredField INSTANCE = new IgnoredField();
+
+}
---
+++ grumpyjson/src/main/java/name/martingeisse/grumpyjson/builtin/helper_types/IgnoredFieldConverter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Martin Geisse
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package name.martingeisse.grumpyjson.builtin.helper_types;
+
+import com.google.gson.JsonElement;
+import name.martingeisse.grumpyjson.JsonRegistries;
+import name.martingeisse.grumpyjson.deserialize.JsonDeserializationException;
+import name.martingeisse.grumpyjson.deserialize.JsonDeserializer;
+import name.martingeisse.grumpyjson.serialize.JsonSerializationException;
+import name.martingeisse.grumpyjson.serialize.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * The converter for {@link IgnoredField}.
+ * <p>
+ * This converter is registered by default, and only needs to be manually registered if it gets removed, such as by
+ * calling {@link JsonRegistries#clear()}.
+ */
+public final class IgnoredFieldConverter implements JsonSerializer<IgnoredField>, JsonDeserializer {
+
+    /**
+     * Constructor.
+     */
+    public IgnoredFieldConverter() {
+    }
+
+    @Override
+    public boolean supportsTypeForDeserialization(Type type) {
+        return type.equals(IgnoredField.class);
+    }
+
+    @Override
+    public IgnoredField deserialize(JsonElement json, Type type) throws JsonDeserializationException {
+        Objects.requireNonNull(json);
+        Objects.requireNonNull(type);
+        return IgnoredField.INSTANCE;
+    }
+
+    @Override
+    public IgnoredField deserializeAbsent(Type type) {
+        Objects.requireNonNull(type);
+        return IgnoredField.INSTANCE;
+    }
+
+    @Override
+    public boolean supportsClassForSerialization(Class<?> clazz) {
+        return clazz.equals(IgnoredField.class);
+    }
+
+    @Override
+    public JsonElement serialize(IgnoredField value) throws JsonSerializationException {
+        Objects.requireNonNull(value);
+        throw new JsonSerializationException("found IgnoredField in a non-vanishable context");
+    }
+
+    @Override
+    public Optional<JsonElement> serializeOptional(IgnoredField value) throws JsonSerializationException {
+        Objects.requireNonNull(value);
+        return Optional.empty();
+    }
+
+}
